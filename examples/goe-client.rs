d44853014d4dbe1b3637d2c8f2b60a3fab588b02
Markus Kasten <github@markuskasten.eu>
2022-08-17 17:53:38 +0200

    feat: add some example code

---
+++ examples/goe-client.rs
@@ -0,0 +1,11 @@
+use go_echarger_api::connection::{http::DirectHttpChargerConnection, GoECharger};
+
+#[tokio::main]
+async fn main() {
+    let mut args = std::env::args();
+    let _ = args.next(); // program name
+    let hostname = args.next().expect("no hostname given");
+
+    let goe = GoECharger::new(DirectHttpChargerConnection::new(hostname));
+    println!("{:?}", goe.latest_status().await.unwrap());
+}
